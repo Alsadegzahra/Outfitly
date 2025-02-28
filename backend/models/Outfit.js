9ac2f1e90b9683d230b335a7c9d90cd04c1820e0
Zahra Alsadeg <zahraalsadeg@Zahras-MBP.lan>
2025-02-27 23:57:26 -0800

    deleted some files

--- backend/models/Outfit.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const OutfitSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Clothing' }],
-    createdAt: { type: Date, default: Date.now } // ✅ Ensure createdAt is set
-});
-
-module.exports = mongoose.model('Outfit', OutfitSchema);
